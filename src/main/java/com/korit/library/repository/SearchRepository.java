5389ffcbc1b47e79dca80fe7e9a6b7f20504c893
smile6060 <dndpgml@naver.com>
2023-02-03 21:52:06 +0900

    no message

--- src/main/java/com/korit/library/repository/SearchRepository.java
+++ src/main/java/com/korit/library/repository/SearchRepository.java
@@ -1,2 +1,8 @@
-package com.korit.library.repository;public class SearchRepository {
+package com.korit.library.repository;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public class SearchRepository {
+
 }
